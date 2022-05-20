b96e324c16a9c06aa96c2db85a73e302f77cd9af
Philip Lages <psilvaelages@gmail.com>
2022-05-20 11:11:30 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,34 @@
 // Desafio 11
 function generatePhoneNumber(numbers) {
-  
-}
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+    };
+  for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < 0 || numbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }    
+    let countNumber = 0;      
+    for (index2 = 0; index2 < numbers.length; index2 += 1) {     
+      if (numbers[index2] === numbers[index]) {        
+        countNumber += 1;      
+        };  
+    }
+      if (countNumber >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      };        
+    };
+  //  Lógica retirada do site https://www.florin-pop.com/blog/2019/04/jcc-create-a-phone-number/
+  let phoneNumber = '(xx) xxxxx-xxxx';       
+  numbers.forEach(item => {
+    phoneNumber = phoneNumber.replace('x', item); 
+  }); 
+    return phoneNumber;
+};   
+
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  
 }
 
 // Desafio 13
